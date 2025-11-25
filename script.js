ac1cef05889cd34ff55dbd256049d5f4f7fa70c9
DarkWingDragoon <darkwingdragoon@gmain.com>
2025-11-25 20:25:24 +0100

    add more dishes in Array myDishes

--- script.js
+++ script.js
@@ -3,7 +3,24 @@ let myDishes = [
     "name": "Spaghetti Carbonara",
     "price": 12.99,
     "description": " Cremige Pasta mit Speck und Parmesan, verfeinert mit Sahne"
+    },
+    {"name": "Margherita Pizza",
+    "price": 10.99,
+    "description": "Klassische Pizza mit Tomatensauce, Mozzarella und frischem Basilikum"
+    },
+    {"name": "Caesar Salad",
+    "price": 8.99,
+    "description": "Frischer Römersalat mit Caesar-Dressing, Croutons und Parmesan"
+    },
+    {"name": "Grilled Chicken",
+    "price": 14.99,
+    "description": "Saftiges gegrilltes Hähnchenbrustfilet mit Kräutern und Gewürzen"
+    },
+    {"name": "Chocolate Lava Cake",
+    "price": 6.99,
+    "description": "Warmer Schokoladenkuchen mit flüssigem Kern, serviert mit Vanilleeis"
     }
+
 ];
 
 
